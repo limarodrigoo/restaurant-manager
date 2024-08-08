7efa4ebf0228df25bc861a50aa5c8a0bf0d8ea05
Rodrigo Lima <limarodrigoo@outlook.com>
2024-08-07 21:53:37 -0300

    lint: fix lint on app module

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,9 +3,18 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { DatabaseModule } from './database/database.module';
 import { ItensModule } from './itens/itens.module';
+import { RestaurantsModule } from './restaurants/restaurants.module';
+import { OrdersModule } from './orders/orders.module';
+import { BillsModule } from './bills/bills.module';
 
 @Module({
-  imports: [DatabaseModule, ItensModule],
+  imports: [
+    DatabaseModule,
+    ItensModule,
+    RestaurantsModule,
+    OrdersModule,
+    BillsModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
