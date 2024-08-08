2cf706afaa66bfc11b13a31e364d9b07aa8ef70e
Rodrigo Lima <limarodrigoo@outlook.com>
2024-08-07 21:53:26 -0300

    fix: remove price orderItens

---
+++ src/prisma/migrations/20240808004125_remove_price/migration.sql
@@ -0,0 +1,8 @@
+/*
+  Warnings:
+
+  - You are about to drop the column `price` on the `OrderItens` table. All the data in the column will be lost.
+
+*/
+-- AlterTable
+ALTER TABLE "OrderItens" DROP COLUMN "price";
