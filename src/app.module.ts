a537c6e6ee256b129c2f991c8bf0c4a756d6cf3d
Rodrigo Lima <limarodrigoo@outlook.com>
2024-08-10 02:36:04 -0300

    feat: create auth middleware

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,4 +1,4 @@
-import { Module } from '@nestjs/common';
+import { MiddlewareConsumer, Module, NestModule } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { DatabaseModule } from './database/database.module';
@@ -6,6 +6,11 @@ import { ItensModule } from './itens/itens.module';
 import { RestaurantsModule } from './restaurants/restaurants.module';
 import { OrdersModule } from './orders/orders.module';
 import { BillsModule } from './bills/bills.module';
+import { AuthMiddleware } from './middelwares/auth.middleware';
+import { ItensController } from './itens/itens.controller';
+import { BillsController } from './bills/bills.controller';
+import { OrdersController } from './orders/orders.controller';
+import { RestaurantsController } from './restaurants/restaurants.controller';
 
 @Module({
   imports: [
@@ -18,4 +23,15 @@ import { BillsModule } from './bills/bills.module';
   controllers: [AppController],
   providers: [AppService],
 })
-export class AppModule {}
+export class AppModule implements NestModule {
+  configure(consumer: MiddlewareConsumer) {
+    consumer
+      .apply(AuthMiddleware)
+      .forRoutes(
+        ItensController,
+        BillsController,
+        OrdersController,
+        RestaurantsController,
+      );
+  }
+}
